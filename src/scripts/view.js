44f5f3333f03783cae34a19364061b778ed0595a
bird <mbird3@gmail.com>
2023-04-02 17:18:23 -0700

    add and test keymaster with k button

--- src/scripts/view.js
+++ src/scripts/view.js
@@ -1,11 +1,17 @@
 import Game from "./game";
+import key from 'keymaster';
 
 export default class View {
 
     constructor(canvasInterface) {
         this.canvasInterface = canvasInterface;
         this.game = new Game(canvasInterface);
+        this.bindKeys();
     }
 
-    
+    bindKeys() {
+        // key('left', () => this.game.ship.power([-1, 0]));
+        key('k', () => console.log("k key pressed"));
+    }
 }
+
